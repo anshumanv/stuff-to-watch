95d66f195428fd491dbe2dec309b454ca2b5ab23
Anshuman <anshu.av97@gmail.com>
2017-12-28 00:55:44 +0530

    Add simple view with sample data

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let express = require('express');
-let todoController = require('./controllers/todoController');
+let stwController = require('./controllers/stwController');
 
 // init app
 let app = express();
+
+// Set templating engine for the app
+app.set('view engine', 'ejs');
 
 // Set assets directory
 app.use(express.static('public'));
 
 // Fire controllers
-todoController(app);
+stwController(app);
 
 // Listen to requests on port 3000
 app.listen(3000);
--- controllers/todoController.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-
-	app.get('/', (req, res) => res.send('init todo'));
-
-};
